642527fd7fe496f08c12911eef662919d942ff20
Matheus Markies <Matheus Markies@DESKTOP-D8ONVGG>
2019-09-16 13:53:51 -0300

    Create TXT commit

---
+++ src/TXT_Utilities/Create_TXT.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TXT_Utilities;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+/**
+ *
+ * @author Matheus Markies
+ */
+public class Create_TXT {
+    
+    File TXT_File = new File("FilePatch\\FileName.txt");
+    
+    public void Create_TXT_File(){
+        
+                     try {
+            TXT_File.createNewFile();
+            FileWriter IndexWriter = null;
+            IndexWriter = new FileWriter(TXT_File.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(IndexWriter);
+            
+            bw.write("Write...");
+            bw.close();  
+            
+             } catch (IOException ex) {
+                 
+             }
+        
+    }
+    
+    
+}
